91b7176e796caa41f2b1a2696118cb5a68a6d388
yuheyan <yyh9695@163.com>
2022-11-21 00:40:43 -0800

    A8

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 import HelloController
     from "./controllers/hello-controller.js"
 import UserController
     from "./controllers/users/users-controller.js"
+import TuitsController
+    from "./controllers/tuits/tuits-controller.js";
 const app = express()
+app.use(cors())
+app.use(express.json());
 HelloController(app)
 UserController(app)
-app.listen(4000)
+TuitsController(app);
+app.listen(process.env.PORT || 4000);
