f33eedacf5156609cc69920d3a60b89009284698
yuheyan <yyh9695@163.com>
2022-11-20 22:35:03 -0800

    part 3

---
+++ app.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import HelloController
+    from "./controllers/hello-controller.js"
+import UserController
+    from "./controllers/users/users-controller.js"
+const app = express()
+HelloController(app)
+UserController(app)
+app.listen(4000)
