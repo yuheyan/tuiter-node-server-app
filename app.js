efff5ebbd1fb70cd42d9c7f67106a60c92a2ca03
yuheyan <yyh9695@163.com>
2022-12-09 16:51:47 -0800

    add env

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ TuitsController(app);
 app.listen(process.env.PORT || 4000);
 
 import mongoose from "mongoose";
+console.log(process.env.DB_CONNECTION_STRING);
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb://localhost:27017/tuiter'
+    || 'mongodb://localhost:27017/tuiter';
+// const  CONNECTION_STRING= 'mongodb+srv://yuheyan:YYH9695@cluster0.enqiatw.mongodb.net/?retryWrites=true&w=majority'
+console.log(CONNECTION_STRING);
 mongoose.connect(CONNECTION_STRING);
